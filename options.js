d85f749d7a8d24098f3790a554d81cb883e7822e
Raynos <raynos2@gmail.com>
2020-04-30 23:03:57 +0200

    cleanup a little

--- options.js
+++ options.js
@@ -3,11 +3,11 @@ var pkg = require('./package.json')
 
 module.exports = {
   // cmd, homepage, bugs all pulled from package.json
-  cmd: 'semistandard',
+  cmd: 'tsdocstandard',
   version: pkg.version,
   homepage: pkg.homepage,
   bugs: pkg.bugs.url,
-  tagline: 'Semicolons For All!',
+  tagline: 'JavaScript + Typescript + JSDoc',
   eslint: require('eslint'),
   eslintConfig: {
     configFile: path.join(__dirname, 'eslintrc.json')
