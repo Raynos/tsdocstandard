af77a437e6f03dc886b83405b86567bc5532d610
Raynos <raynos2@gmail.com>
2020-10-07 17:39:05 +0200

    support jsconfig.json

--- options.js
+++ options.js
@@ -20,17 +20,29 @@ module.exports = {
 
     const cwd = opts.cwd || process.cwd()
     opts.eslintConfig.parserOptions.tsconfigRootDir = cwd
-    opts.eslintConfig.parserOptions.project = './tsconfig.json'
 
-    const tsconfigText = fs.readFileSync(
-      path.join(cwd, 'tsconfig.json')
-    )
-    const obj = json5.parse(tsconfigText)
-    if (!obj.compilerOptions || !obj.compilerOptions.allowJs) {
-      throw new Error("Expected your tsconfig.json to contain \"allowJs\": true")
-    }
-    if (!obj.compilerOptions || !obj.compilerOptions.checkJs) {
-      throw new Error("Expected your tsconfig.json to contain \"checkJs\": true")
+    const tsExist = fs.existsSync(path.join(cwd, 'tsconfig.json'))
+    const jsExists = fs.existsSync(path.join(cwd, 'jsconfig.json'))
+
+    let fileName = null
+    if (tsExist) fileName = 'tsconfig.json'
+    else if (jsExists) fileName = 'jsconfig.json'
+
+    if (fileName) {
+      opts.eslintConfig.parserOptions.project = `./${fileName}`
+
+      const tsconfigText = fs.readFileSync(
+        path.join(cwd, fileName)
+      )
+      const obj = json5.parse(tsconfigText)
+      if (!obj.extends) {
+        if (!obj.compilerOptions || !obj.compilerOptions.allowJs) {
+          throw new Error("Expected your tsconfig.json to contain \"allowJs\": true")
+        }
+        if (!obj.compilerOptions || !obj.compilerOptions.checkJs) {
+          throw new Error("Expected your tsconfig.json to contain \"checkJs\": true")
+        }
+      }
     }
 
     if (opts.filename && opts.filename.indexOf('file://') === 0) {
